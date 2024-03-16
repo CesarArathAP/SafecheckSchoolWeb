17bb23f95a16fb47fe02a5200399132bb70f4176
CesarArathAP <1722110585@utectulancingo.edu.mx>
2024-03-16 09:54:40 -0600

    se agrego una validacion de id al controlador lista_alumnos

--- safecheck_web/mvc/controllers/lista_alumnos.py
+++ safecheck_web/mvc/controllers/lista_alumnos.py
@@ -1,4 +1,3 @@
-# lista_alumnos.py
 import web
 from mvc.models.safecheck import EspecialidadesModel
 
@@ -6,9 +5,10 @@ render = web.template.render('mvc/views/', base="layout")
 
 class ListaAlumnos:
     def GET(self, especialidad_id):
-        # Convertir el especialidad_id a un tipo adecuado, como str o int
-        especialidad_id = float(especialidad_id)
-        
-        model = EspecialidadesModel()
-        alumnos = model.get_alumnos_by_especialidad(especialidad_id)
-        return render.lista_alumnos(alumnos=alumnos)
+        try:
+            especialidad_id = float(especialidad_id)
+            model = EspecialidadesModel()
+            alumnos = model.get_alumnos_by_especialidad(especialidad_id)
+            return render.lista_alumnos(alumnos=alumnos)
+        except ValueError:
+            return "Especialidad ID inválido"
