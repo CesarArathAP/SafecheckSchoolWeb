a4ee1d5d2ce69f5f77b3551762e4c7d81140fe28
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2024-03-17 17:37:46 -0600

    Update vigilancia_registration.py
    ya registra a un policia

--- safecheck_web/mvc/controllers/vigilancia_registration.py
+++ safecheck_web/mvc/controllers/vigilancia_registration.py
@@ -16,11 +16,10 @@ class VigilanciaRegistration:
         username = form.username
         password = form.password
 
-        # Llama al método correspondiente en tu modelo para registrar al personal de vigilancia
-        if model.registrar_vigilancia(nombre, apellidos, telefono, username, password):
-            # Si el registro fue exitoso, redirige a la página "new"
-            raise web.seeother('/new')
+        # Intenta registrar al personal de vigilancia en la base de datos
+        if model.registrar_policia(nombre, apellidos, telefono, username, password):
+            # Si el registro fue exitoso, redirige a alguna página de éxito
+            raise web.seeother('/new')  # Cambia '/exito' por la URL que desees
         else:
             # Si ocurrió un error, muestra un mensaje de error o redirige a alguna página de error
             raise web.seeother('/error')  # Cambia '/error' por la URL que desees
-
