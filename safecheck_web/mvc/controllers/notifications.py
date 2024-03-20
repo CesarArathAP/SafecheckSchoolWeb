4b80159651e7367df394193e6c0fc3de85a90867
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2024-03-20 09:49:53 -0600

    Update notifications.py

--- safecheck_web/mvc/controllers/notifications.py
+++ safecheck_web/mvc/controllers/notifications.py
@@ -1,7 +1,11 @@
 import web
+from mvc.models.safecheck import SafeCheck
 
 render = web.template.render('mvc/views/', base="layout")
 
 class Notifications:
     def GET(self):
-        return render.notifications()
+        model = SafeCheck()
+        reportes = model.obtener_reportes()  # Obtener los datos de las visitas desde el modelo
+        return render.notifications(reportes=reportes)  # Pasar los datos de las visitas a la plantilla
+    
