5d4d10c8427c689cfe6ecd9a7bb15814e2dcb178
Miguel Ángel López Hernández <122801484+MiguelAngelLH@users.noreply.github.com>
2024-04-16 07:55:04 -0600

    Delete static directory

--- static/js/updateSelectedCarreras.js
+++
@@ -1,8 +0,0 @@
-function updateSelectedCarreras() {
-    var carrerasSeleccionadas = '';
-    var checkboxes = document.querySelectorAll('input[name="carrera[]"]:checked');
-    checkboxes.forEach(function(checkbox) {
-        carrerasSeleccionadas += checkbox.nextElementSibling.textContent + '\n';
-    });
-    document.getElementById('carreraSeleccionada').value = carrerasSeleccionadas;
-}
